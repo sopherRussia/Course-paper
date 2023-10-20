ffdae4cef918cfe8a8e1400533d741864df857b0
sopherRussia <148501860+sopherRussia@users.noreply.github.com>
2023-10-21 01:13:49 +0300

    Add files via upload

---
+++ Course paper/Course paper/By_the_user.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Course_paper
+{
+    /// <summary>
+    /// Логика взаимодействия для By_the_user.xaml
+    /// </summary>
+    public partial class By_the_user : Window
+    {
+        public By_the_user()
+        {
+            InitializeComponent();
+        }
+    }
+}
